be4b83bd13e7a9941d526f2b136cbae28b4b953e
Ortniter <mr.bob.bigg@gmail.com>
2020-08-07 16:38:06 +0300

    added get_html and get_response methods

--- yahoo_finance_task.py
+++ yahoo_finance_task.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+
+import requests
 from selenium.webdriver.firefox.options import Options
 
 
@@ -12,3 +14,17 @@ class YahooFinanceBaseParser:
         self.driver = webdriver.Firefox(options=options)
         self.companies = companies
         self.base_url = 'https://finance.yahoo.com'
+
+    def get_html(self, url):
+        raise NotImplementedError('Use parse_html method from child')
+
+    @staticmethod
+    def get_response(url):
+        """
+        :param url: takes url
+        :return: response from url or False if status_code == 200
+        """
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response
+        return False
